a732cf41e0ad26777036c62e3b8f62f7d538de7c
Maxime Ghignet <maxime.ghignet@gmail.com>
2014-08-27 02:24:52 +0200

    refactor: update libraries and use a better lazylist lib

--- app/src/main/java/com/mghignet/android/ts3viewer/adapter/ChannelAdapter.java
+++ app/src/main/java/com/mghignet/android/ts3viewer/adapter/ChannelAdapter.java
@@ -1,7 +1,5 @@
 package com.mghignet.android.ts3viewer.adapter;
 
-import java.util.List;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,76 +7,73 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.mghignet.android.ts3viewer.R;
 import com.mghignet.android.ts3viewer.domain.Channel;
-import com.mghignet.android.ts3viewer.lazylist.ImageLoader;
 import com.mghignet.android.ts3viewer.util.CalculatorUtil;
+import com.squareup.picasso.Picasso;
+import java.util.List;
 
 public class ChannelAdapter extends ArrayAdapter<Channel> {
-   
-	private List<Channel> channels;
-    private LayoutInflater layoutInflater;
-    private ChannelViewHolder holder;
-    private ImageLoader imageLoader;
-    private Context context;
 
-    public ChannelAdapter(Context context, int textViewResourceId, List<Channel> channels) {
-    	super(context, textViewResourceId);
-    	this.context = context;
-        this.layoutInflater = LayoutInflater.from(context);
-        this.channels = channels;
-        imageLoader = new ImageLoader(context);
+  private List<Channel> channels;
+  private LayoutInflater layoutInflater;
+  private ChannelViewHolder holder;
+  private Context context;
+
+  public ChannelAdapter(Context context, int textViewResourceId, List<Channel> channels) {
+    super(context, textViewResourceId);
+    this.context = context;
+    this.layoutInflater = LayoutInflater.from(context);
+    this.channels = channels;
+  }
+
+  public int getCount() {
+    return channels.size();
+  }
+
+  public Channel getItem(int position) {
+    return channels.get(position);
+  }
+
+  public long getItemId(int id) {
+    return id;
+  }
+
+  public View getView(int position, View convertView, ViewGroup parent) {
+    if (convertView == null) {
+      convertView = layoutInflater.inflate(R.layout.list_item_channel, parent, false);
+      holder = new ChannelViewHolder();
+      holder.itemLayout = (View) convertView.findViewById(R.id.item_layout);
+      holder.nameTextView = (TextView) convertView.findViewById(R.id.name);
+      holder.statusImageView = (ImageView) convertView.findViewById(R.id.status_image);
+      holder.flagImageView = (ImageView) convertView.findViewById(R.id.flag_image);
+      convertView.setTag(holder);
+    } else {
+      holder = (ChannelViewHolder) convertView.getTag();
     }
 
-    public int getCount() {
-        return channels.size();
+    holder.channel = channels.get(position);
+
+    int paddingLeft = CalculatorUtil.getInstance(context).pxFromDp(20 + 20 * holder.channel.getTreeLevel());
+    int paddingOther = CalculatorUtil.getInstance(context).pxFromDp(10);
+    holder.itemLayout.setPadding(paddingLeft, paddingOther, paddingOther, paddingOther);
+
+    holder.nameTextView.setText(holder.channel.getName());
+    Picasso.with(context).load(holder.channel.getStatusImage()).into(holder.statusImageView);
+    Picasso.with(context).load(holder.channel.getFlagImage()).into(holder.flagImageView);
+
+    return convertView;
+  }
+
+  public class ChannelViewHolder {
+    View itemLayout;
+    TextView nameTextView;
+    ImageView statusImageView;
+    ImageView flagImageView;
+    Channel channel;
+
+    public Channel getChannel() {
+      return channel;
     }
-
-    public Channel getItem(int position) {
-        return channels.get(position);
-    }
-
-    public long getItemId(int id) {
-        return id;
-    }
-
-    public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            convertView = layoutInflater.inflate(R.layout.list_item_channel, null);
-            holder = new ChannelViewHolder();
-            holder.itemLayout = (View) convertView.findViewById(R.id.item_layout);
-            holder.nameTextView = (TextView) convertView.findViewById(R.id.name);
-            holder.statusImageView = (ImageView) convertView.findViewById(R.id.status_image);
-            holder.flagImageView = (ImageView) convertView.findViewById(R.id.flag_image);
-            convertView.setTag(holder);
-        } else {
-            holder = (ChannelViewHolder) convertView.getTag();
-        }
-        
-        holder.channel = channels.get(position);
-
-        int paddingLeft = CalculatorUtil.getInstance(context).pxFromDp(20 + 20 * holder.channel.getTreeLevel());
-        int paddingOther = CalculatorUtil.getInstance(context).pxFromDp(10);
-        holder.itemLayout.setPadding(paddingLeft, paddingOther, paddingOther, paddingOther);
-        
-        holder.nameTextView.setText(holder.channel.getName());
-        imageLoader.DisplayImage(holder.channel.getStatusImage(), holder.statusImageView);
-        imageLoader.DisplayImage(holder.channel.getFlagImage(), holder.flagImageView);
-
-        return convertView;
-    }
-
-    public class ChannelViewHolder
-    {
-    	View itemLayout;
-        TextView nameTextView;
-        ImageView statusImageView;
-        ImageView flagImageView;
-        Channel channel;
-
-        public Channel getChannel() {
-            return channel;
-        }
-    }
+  }
 }
--- app/src/main/java/com/mghignet/android/ts3viewer/adapter/ServerAdapter.java
+++ app/src/main/java/com/mghignet/android/ts3viewer/adapter/ServerAdapter.java
@@ -1,7 +1,5 @@
 package com.mghignet.android.ts3viewer.adapter;
 
-import java.util.List;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,64 +7,63 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.mghignet.android.ts3viewer.R;
 import com.mghignet.android.ts3viewer.domain.Server;
-import com.mghignet.android.ts3viewer.lazylist.ImageLoader;
+import com.squareup.picasso.Picasso;
+import java.util.List;
 
 public class ServerAdapter extends ArrayAdapter<Server> {
 
-    private List<Server> servers;
-    private LayoutInflater layoutInflater;
-    private ServerViewHolder holder;
-    private ImageLoader imageLoader;
+  private List<Server> servers;
+  private LayoutInflater layoutInflater;
+  private ServerViewHolder holder;
+  private Context context;
 
-    public ServerAdapter(Context context, int textViewResourceId, List<Server> servers) {
-    	super(context, textViewResourceId);
-        this.layoutInflater = LayoutInflater.from(context);
-        this.servers = servers;
-        imageLoader = new ImageLoader(context);
+  public ServerAdapter(Context context, int textViewResourceId, List<Server> servers) {
+    super(context, textViewResourceId);
+    this.layoutInflater = LayoutInflater.from(context);
+    this.context = context;
+    this.servers = servers;
+  }
+
+  public int getCount() {
+    return servers.size();
+  }
+
+  public Server getItem(int position) {
+    return servers.get(position);
+  }
+
+  public long getItemId(int id) {
+    return id;
+  }
+
+  public View getView(int position, View convertView, ViewGroup parent) {
+    if (convertView == null) {
+      convertView = layoutInflater.inflate(R.layout.list_item_server, parent, false);
+      holder = new ServerViewHolder();
+      holder.nameTextView = (TextView) convertView.findViewById(R.id.name);
+      holder.statusImageView = (ImageView) convertView.findViewById(R.id.status_image);
+      convertView.setTag(holder);
+    } else {
+      holder = (ServerViewHolder) convertView.getTag();
     }
 
-    public int getCount() {
-        return servers.size();
+    holder.server = servers.get(position);
+
+    holder.nameTextView.setText(holder.server.getName());
+    Picasso.with(context).load(holder.server.getStatusImage()).into(holder.statusImageView);
+
+    return convertView;
+  }
+
+  public class ServerViewHolder {
+    TextView nameTextView;
+    ImageView statusImageView;
+    Server server;
+
+    public Server getServer() {
+      return server;
     }
-
-    public Server getItem(int position) {
-        return servers.get(position);
-    }
-
-    public long getItemId(int id) {
-        return id;
-    }
-
-    public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            convertView = layoutInflater.inflate(R.layout.list_item_server, null);
-            holder = new ServerViewHolder();
-            holder.nameTextView = (TextView) convertView.findViewById(R.id.name);
-            holder.statusImageView = (ImageView) convertView.findViewById(R.id.status_image);
-            convertView.setTag(holder);
-        } else {
-            holder = (ServerViewHolder) convertView.getTag();
-        }
-        
-        holder.server = servers.get(position);
-        
-    	holder.nameTextView.setText(holder.server.getName());
-        imageLoader.DisplayImage(holder.server.getStatusImage(), holder.statusImageView);
-
-        return convertView;
-    }
-
-    public class ServerViewHolder
-    {
-        TextView nameTextView;
-        ImageView statusImageView;
-        Server server;
-
-        public Server getServer() {
-            return server;
-        }
-    }
+  }
 }
